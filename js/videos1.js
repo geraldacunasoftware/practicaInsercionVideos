6177dbd5c4d4094e6d07a2f1010f37dee60e2554
gerald <geraldacunasoftware@gmail.com>
2024-12-17 07:23:43 -0600

    modificación en archivo js barra de progreso

--- js/videos1.js
+++ js/videos1.js
@@ -1,7 +1,7 @@
 function Video(){        
     
-    var video,play,barra,progreso;
-    
+    var video,play,barra,progreso,maximo;
+    maximo = 600;
     this.reproductor = () => {
     
      video = document.getElementById('video');
@@ -10,21 +10,35 @@ function Video(){
      progreso = document.getElementById('progreso');
     
      play.addEventListener('click',this.reproducir);
-   /* 
-    progreso.addEventListener('click',adelantos,false);
-    */
+    
+     barra.addEventListener('click',this.adelantos,false);
+    
     }
     this.reproducir = () =>{
         console.log(video)
-        if ((video.paused == false) && (video.ended== false)) {
+        if ((video.paused == false) && (video.ended)== false) {
             video.pause();
             play.innerHTML="Play"
         }else{
             video.play();
             play.innerHTML="Pause"
+            bucle=setInterval(this.estado,25);
         }
     }    
-
+    this.estado = () =>{
+        if (video.ended==false) {
+            var total = parseInt(video.currentTime*maximo/video.duration);
+            progreso.style.width=total+"px"
+        }
+    }
+    this.adelantos = (posicion)=>{
+        if((video.paused == false) && (video.ended)==false){
+            var posicionX = posicion.pageX-barra.offsetLeft;
+            var nuevoTiempo = posicionX*video.duration/maximo;
+            video.currentTime=nuevoTiempo;
+            progreso.style.width=posicionX+"px";
+        }
+    }
 }
 document.addEventListener('DOMContentLoaded',()=>{
     const pagina = new Video();
