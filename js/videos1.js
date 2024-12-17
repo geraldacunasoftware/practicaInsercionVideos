d842fa505c18d30852ce917fed0f9f7b11b40830
gerald <geraldacunasoftware@gmail.com>
2024-12-16 18:11:56 -0600

    Practica de inserción de videos html,css y js

---
+++ js/videos1.js
@@ -0,0 +1,32 @@
+function Video(){        
+    
+    var video,play,barra,progreso;
+    
+    this.reproductor = () => {
+    
+     video = document.getElementById('video');
+     play = document.getElementById('play');
+     barra = document.getElementById('barra');
+     progreso = document.getElementById('progreso');
+    
+     play.addEventListener('click',this.reproducir);
+    
+    progreso.addEventListener('click',adelantos,false);
+    
+    }
+    this.reproducir = () =>{
+        console.log(video)
+        if ((video.paused == false) && (video.ended== false)) {
+            video.pause();
+            play.innerHTML="Play"
+        }else{
+            video.play();
+            play.innerHTML="Pause"
+        }
+    }    
+
+}
+document.addEventListener('DOMContentLoaded',()=>{
+    const pagina = new Video();
+    pagina.reproductor();
+});
